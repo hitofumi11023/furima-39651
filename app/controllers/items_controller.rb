23d382fdf0c95dd766ba3b9688b90eeb7dc5150c
hitofumi11023 <hitofumi123@gmail.com>
2023-09-03 10:10:34 +0900

    商品詳細表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,25 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    unless  current_user.id == @item.user.id
+      redirect_to root_path
+    end
+    unless user_signed_in?  
+      redirect_to new_user_session_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def create
     @item = Item.new(item_params)
     if @item.save
