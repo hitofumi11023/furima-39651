594f24e561e517f567e564f69a79379a8434298c
hitofumi11023 <hitofumi123@gmail.com>
2023-09-05 19:30:59 +0900

    修正

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -1,13 +1,11 @@
 FactoryBot.define do
   factory :purchase_address do
-    user_id       { 1 }
-    item_id       { 1 }
     postal_code   { '123-1234' }
     prefecture_id { 2 }
     city          { '山田市' }
     addresses     { '山田町1-1' }
-    building      { '山田ビル'}
+    building      { '山田ビル' }
     phone_number  { '0000000000' }
-    token         {"tok_abcdefghijk00000000000000000"}
+    token         { "tok_abcdefghijk00000000000000000" }
   end
 end
--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe PurchaseAddress, type: :model do
   before do
-    @purchase_address = FactoryBot.build(:purchase_address)
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @purchase_address = FactoryBot.build(:purchase_address, user_id: user.id, item_id: item.id)
   end
   
   describe '商品購入情報の保存' do
