4e97f569e43c807e573029b30e402c122fb14d49
hitofumi11023 <hitofumi123@gmail.com>
2023-10-30 13:30:00 +0900

    複数枚画像投稿機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -57,7 +57,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :item_info, :category_id, :sales_status_id, :shipping_fee_id, :prefecture_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :item_info, :category_id, :sales_status_id, :shipping_fee_id, :prefecture_id, :scheduled_delivery_id, :price, {images: []}).merge(user_id: current_user.id)
   end
 
 end
--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,37 +1,76 @@
-document.addEventListener('turbo:load', function(){
-  // 新規投稿・編集ページのフォームを取得
-  const postForm = document.getElementById('item-image');
-
-  // プレビューを表示するためのスペースを取得
+document.addEventListener('DOMContentLoaded', function(){
+  const postForm = document.getElementById('new_post');
   const previewList = document.getElementById('previews');
-
-  // 新規投稿・編集ページのフォームがないならここで終了
+  
   if (!postForm) return null;
 
-  // input要素を取得
-  const fileField = document.querySelector('input[type="file"][name="item[image]"]');
-  // input要素で値の変化が起きた際に呼び出される関数
-  fileField.addEventListener('change', function(e){
-    // 古いプレビューが存在する場合は削除
-    const alreadyPreview = document.querySelector('.preview');
-    if (alreadyPreview) {
-      alreadyPreview.remove();
-    };
+  const imageLimits = 5;
+
+  const buildPreviewImage = (dataIndex, blob) =>{
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class', 'preview');
+    previewWrapper.setAttribute('data-index', dataIndex);
+
+    const previewImage= document.createElement('img');
+    previewImage.setAttribute('class', 'preview-image');
+    previewImage.setAttribute('src', blob);
+
+    const deleteButton = document.createElement("div");
+    deleteButton.setAttribute("class", "image-delete-button");
+    deleteButton.innerText = "削除";
+
+    deleteButton.addEventListener("click", () => deleteImage(dataIndex));
+
+    previewWrapper.appendChild(previewImage);
+    previewWrapper.appendChild(deleteButton);
+    previewList.appendChild(previewWrapper);
+  };
+
+  const buildNewFileField = () => {
+    const newFileField = document.createElement('input');
+    newFileField.setAttribute('type', 'file');
+    newFileField.setAttribute('name', 'item[images][]');
+
+    const lastFileField = document.querySelector('input[type="file"][name="item[images][]"]:last-child');
+    const nextDataIndex = Number(lastFileField.getAttribute('data-index')) +1;
+    newFileField.setAttribute('data-index', nextDataIndex);
+
+    newFileField.addEventListener("change", changedFileField);
+
+    const fileFieldsArea = document.querySelector('.click-upload');
+    fileFieldsArea.appendChild(newFileField);
+  };
+
+  const deleteImage = (dataIndex) => {
+    const deletePreviewImage = document.querySelector(`.preview[data-index="${dataIndex}"]`);
+    deletePreviewImage.remove();
+    const deleteFileField = document.querySelector(`input[type="file"][data-index="${dataIndex}"]`);
+    deleteFileField.remove();
+
+    const imageCount = document.querySelectorAll(".preview").length;
+    if (imageCount == imageLimits - 1) buildNewFileField();
+  };
+
+  const changedFileField = (e) => {
+    const dataIndex = e.target.getAttribute('data-index');
 
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
 
-    // 画像を表示するためのdiv要素を生成
-    const previewWrapper = document.createElement('div');
-    previewWrapper.setAttribute('class', 'preview');
+    const alreadyPreview = document.querySelector(`.preview[data-index="${dataIndex}"]`);
 
-    // 表示する画像を生成
-    const previewImage = document.createElement('img');
-    previewImage.setAttribute('class', 'preview-image');
-    previewImage.setAttribute('src', blob);
+    if (alreadyPreview) {
+      const alreadyPreviewImage = alreadyPreview.querySelector("img");
+      alreadyPreviewImage.setAttribute("src", blob);
+      return null;
+    };
 
-    // 生成したHTMLの要素をブラウザに表示させる
-    previewWrapper.appendChild(previewImage);
-    previewList.appendChild(previewWrapper);
-  });
+    buildPreviewImage(dataIndex, blob);
+    const imageCount = document.querySelectorAll(".preview").length;
+    if (imageCount < imageLimits) buildNewFileField();
+  };
+
+  const fileField = document.querySelector('input[type="file"][name="item[images][]"]');
+
+  fileField.addEventListener('change', changedFileField);
 });
